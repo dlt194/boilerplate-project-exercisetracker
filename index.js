2a0db3fe3e651dc56a566be7056f6f68cf74bc33
@dlt194 <36240712+dlt194@users.noreply.github.com>
2024-10-28 21:39:03 +0000

    Tests #1, #2, #3, #4, #5 & #6

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+let bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+let mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const Schema = mongoose.Schema;
 
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: String,
+});
 
+const userSchema = new Schema({
+  username: String,
+});
 
+const exerciseModel = mongoose.model("exercise", exerciseSchema);
+const userModel = mongoose.model("user", userSchema);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/api/users", (req, res) => {
+  userModel
+    .find({})
+    .then(function (users) {
+      res.json(users);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.json({ error: err });
+    });
+});
+
+app.post("/api/users/", (req, res) => {
+  username = req.body.username;
+  newUser = new userModel({
+    username: username,
+  });
+
+  newUser
+    .save((user) => {
+      res.json({ username: user.username, _id: user._id });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.json({ error: err });
+    });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
