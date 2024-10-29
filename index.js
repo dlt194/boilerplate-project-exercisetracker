c9925d023de6e6f17bd9a57aa22706462ca986c8
@dlt194 <36240712+dlt194@users.noreply.github.com>
2024-10-29 15:12:19 +0000

    Working code

--- index.js
+++ index.js
@@ -1,85 +1,84 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
-let bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-let mongoose = require("mongoose");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const Schema = mongoose.Schema;
-
 const exerciseSchema = new mongoose.Schema({
-  id: String,
+  userId: String,
   username: String,
   description: { type: String, required: true },
   duration: { type: Number, required: true },
   date: String,
 });
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   username: String,
 });
 
-const exerciseModel = mongoose.model("exercise", exerciseSchema);
-const userModel = mongoose.model("user", userSchema);
+let User = mongoose.model("User", userSchema);
+let Exercise = mongoose.model("Exercise", exerciseSchema);
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static("public"));
-app.get("/", (req, res) => {
+app.get("/", async (_req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+  await User.syncIndexes();
+  await Exercise.syncIndexes();
 });
 
-app.get("/api/users", (req, res) => {
-  userModel
-    .find({})
-    .then(function (users) {
+app.get("/api/users", function (_req, res) {
+  User.find({})
+    .then((users) => {
       res.json(users);
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.error(err);
       res.json({ error: err });
     });
 });
 
-app.post("/api/users", (req, res) => {
-  username = req.body.username;
-  newUser = new userModel({
-    username: username,
-  });
+app.post("/api/users", function (req, res) {
+  const username = req.body.username;
+
+  let newUser = new User({ username: username });
 
   newUser
     .save()
     .then((user) => {
       res.json({ username: user.username, _id: user._id });
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.error(err);
       res.json({ error: err });
     });
 });
 
-app.post("/api/users/:id/exercises", (req, res) => {
-  id = req.params.id;
-  description = req.body.description;
-  duration = req.body.duration;
-  date = req.body.date;
+app.post("/api/users/:_id/exercises", (req, res) => {
+  var userId = req.params._id;
+  var description = req.body.description;
+  var duration = req.body.duration;
+  var date = req.body.date;
 
   if (!date) {
-    date = new Date().toDateString();
+    date = new Date().toISOString().substring(0, 10);
   }
 
-  userModel
-    .findById(id)
-    .then((user) => {
-      newExercise = new exerciseModel({
-        id: user.id,
-        username: user.username,
+  User.findById(userId)
+    .then((userInDb) => {
+      let newExercise = new Exercise({
+        userId: userInDb._id,
+        username: userInDb.username,
         description: description,
         duration: parseInt(duration),
         date: date,
@@ -89,38 +88,37 @@ app.post("/api/users/:id/exercises", (req, res) => {
         .save()
         .then((exercise) => {
           res.json({
-            username: user.username,
+            username: userInDb.username,
             description: exercise.description,
             duration: exercise.duration,
             date: new Date(exercise.date).toDateString(),
-            _id: user._id,
+            _id: userInDb._id,
           });
         })
-        .catch(function (err) {
+        .catch((err) => {
           console.error(err);
           res.json({ error: err });
         });
     })
-    .catch(function (err) {
+    .catch((err) => {
       console.error(err);
       res.json({ error: err });
     });
 });
 
-app.get("/api/users/:_id/logs", async function (req, res) {
-  id = req.params._id;
-  from = req.query.from || new Date(0).toDateString();
-  to = req.query.to || new Date(Date.now()).toDateString();
-  limit = Number(req.query.limit) || 0;
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const userId = req.params._id;
+  const from = req.query.from || new Date(0).toISOString().substring(0, 10);
+  const to =
+    req.query.to || new Date(Date.now()).toISOString().substring(0, 10);
+  const limit = Number(req.query.limit) || 0;
 
-  user = await userModel.findById(id).exec();
+  let user = await User.findById(userId).exec();
 
-  //? Find the exercises
-  let exercises = await exerciseModel
-    .find({
-      id: id,
-      date: { $gte: from, $lte: to },
-    })
+  let exercises = await Exercise.find({
+    userId: userId,
+    date: { $gte: from, $lte: to },
+  })
     .select("description duration date")
     .limit(limit)
     .exec();
